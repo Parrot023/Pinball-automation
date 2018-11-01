9715e6e83835e9ea6b4d038005b9e5498a5500ec
Parrot023 <Andreas081203@gmail.com>
2018-11-01 17:38:23 +0100

    added on/off delay

--- FlipperMachineProject/FlipperMachineProject.ino
+++ FlipperMachineProject/FlipperMachineProject.ino
@@ -1,44 +1,113 @@
-const int sensorPin = 3;
+//sensor pins
+const int sensorPin1 = 2;
+const int sensorPin2 = 3;
+
+//counting every time the sensors are activated
 int count = 0;
 int prev_count;
-const int ledPin = 4;
-int led_on_time = 1000;
-int led_delay = 1000;
-boolean right_trig;
+
+//led pins
+const int ledPin1 = 5;
+const int ledPin2 = 4;
+
+//right and left trigger time left counting from max_time
+volatile unsigned int left_flipper_time_left;
+volatile unsigned int right_flipper_time_left;
+
+//delays
+//unsigned integer can max be 64000
+const unsigned int max_time = 60000;
+const unsigned int delay_time = 30000;
+
 
 void setup() {
-  // Sets the sensorPin, as an Input
-  pinMode(sensorPin, INPUT);
-  pinMode(ledPin, OUTPUT);
   
-  // Attaches sensorPin as an interrupt, and calls the function sensorHigh, when sensorPin becomes HIGH
-  attachInterrupt(digitalPinToInterrupt(sensorPin), sensorHigh, RISING);
+  //declare pin modes
+  pinMode(sensorPin1, INPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(sensorPin2, INPUT);
+  pinMode(ledPin2, OUTPUT);
   
-  // Starts the serial communication
+  //attaches sensorPin as an interrupt, and calls the function sensorHigh, when sensorPin becomes HIGH
+  attachInterrupt(digitalPinToInterrupt(sensorPin1), right_trig_interrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(sensorPin2), left_trig_interrupt, RISING);
+  
+  //starts the serial communication
   Serial.begin(9600);
 
 }
 
 void loop() {
 
-  // Prints message, if sensorState is high
+  // prints number of sensor activations if changed
   if (prev_count != count) {
     Serial.println(count);
     prev_count = count;
   }
 
-  //right flip
-  if (right_trig) {
-    delay(led_delay);
-    digitalWrite(ledPin, HIGH);
-    delay(led_on_time); 
-    digitalWrite(ledPin, LOW);
-    right_trig = false;
-  }
-}
+  //right flipper
+  //with delay
+  //if -flipper_time_left = higher than 0 countdown from max_time
+  //if -flipper_time_left = equal to max_time - delay_time: turn on
+  //if -flipper_time_left = equall to 0 turn off
 
-// Function which is called by sensorPin interrupt
-void sensorHigh() {
+  if (right_flipper_time_left == max_time - delay_time) {
+    digitalWrite(ledPin1, HIGH);
+  }
+  
+  if (right_flipper_time_left == 0) {
+    digitalWrite(ledPin1, LOW);
+  }
+
+  if (right_flipper_time_left > 0){
+    right_flipper_time_left--;
+  }
+
+
+  //left flipper
+  //with delay 
+  
+   if (left_flipper_time_left == max_time - delay_time) {
+    digitalWrite(ledPin2, HIGH);
+  }
+  
+  if (left_flipper_time_left == 0) {
+    digitalWrite(ledPin2, LOW);
+  }
+
+  if (left_flipper_time_left > 0){
+    left_flipper_time_left--;
+  }
+
+
+// without delay  
+//  if (right_flipper_time_left > 0){
+//    digitalWrite(ledPin1, HIGH);
+//    right_flipper_time_left--;
+//  } else {
+//    digitalWrite(ledPin1, LOW);
+//  }
+
+//  //left flipper
+//  if (left_flipper_time_left > 0){
+//    digitalWrite(ledPin2, HIGH);
+//    left_flipper_time_left--;
+//  } else {
+//    digitalWrite(ledPin2, LOW);
+//  }
+//
+} //end of main loop
+    
+ 
+
+//function which is called by sensorPin1 interrupt
+void right_trig_interrupt() {
   count++;
-  right_trig = true;
+  right_flipper_time_left = max_time;
+  
+}
+//function which is called by sensorPin2 interrupt
+void left_trig_interrupt() {
+  count++;
+  left_flipper_time_left = max_time;
 }
