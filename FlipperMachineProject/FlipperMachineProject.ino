12cb46a377b2792723349d73025ff0e57dc9d826
Parrot023 <Andreas081203@gmail.com>
2019-03-21 17:29:43 +0100

    Added 3D model measurements to README

--- FlipperMachineProject/FlipperMachineProject.ino
+++ FlipperMachineProject/FlipperMachineProject.ino
@@ -1,3 +1,10 @@
+//info 
+//diodes: 1N914
+//
+
+
+
+
 //delays
 const int flipperUpTime = 100;
 const int flipperWaitTime = 1000;
@@ -11,21 +18,21 @@ int count = 0;
 int prev_count;
 
 //flipper pins
-const int rightflipper = 5;
-const int leftflipper = 4;
+const int rightflipper = 4;
+const int leftflipper = 5;
 
 
 
 
 void setup() { //------------------------------------------------------------------------------------------------------------>
-  
+
   //declare pin modes
   pinMode(rightsensor, INPUT);
   pinMode(rightflipper, OUTPUT);
-  
+
   pinMode(leftsensor, INPUT);
   pinMode(leftflipper, OUTPUT);
-  
+
   //starts the serial communication
   Serial.begin(9600);
 
@@ -40,20 +47,21 @@ void loop() { //----------------------------------------------------------------
 
 
 
-  
+
   //right flipper ------------------------------------------------------------------------------->
-  
+
     if (digitalRead(rightsensor) == HIGH) {
 
       //activate flipper
       digitalWrite(rightflipper, HIGH);
-      
+      Serial.println("sjifhiojhfjih");
+
       //this delay is needed by the flipper, if this delay wasnt here the flipper would not flip
       delay(flipperUpTime);
-      
+
       //deactivate flipper
       digitalWrite(rightflipper, LOW);
-      
+
       //delay to prevent the flipper from activating itself,
       //this might have to be redone in the future for support for multiple sensors per flipper
       //maybe using interrupts again
@@ -62,17 +70,18 @@ void loop() { //----------------------------------------------------------------
       //counts number of sensor activations
       count ++;
   }
-  
+
   //left flipper --------------------------------------------------------------------------------->
-  
+
    if (digitalRead(leftsensor) == HIGH) {
-    
+
      //activates flipper
      digitalWrite(leftflipper, HIGH);
-     
-     //this delay is needed by the flipper, if this delay wasnt here the flipper would not flip 
+     Serial.println("ksjdhgksjdhgkjh");
+
+     //this delay is needed by the flipper, if this delay wasnt here the flipper would not flip
      delay(flipperUpTime);
-     
+
      //deactivate flipper
      digitalWrite(leftflipper, LOW);
 
@@ -85,5 +94,5 @@ void loop() { //----------------------------------------------------------------
      count++;
   }
 
-  
-} //end of main loop --------------------------------------------------------------------------------------------------------------------->
+
+} //end of main loop ----------------------------------------------------------------------------------------------------------->
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 This project is about turning an old classic flipper machince, into an automatic flipper beast
+
+
+3D print info:
+
+Body depth: 10 mm.
+Body height: 7 mm.
+Body lenght: is defined by number number of repeats.
+
+divider (the wall between each sensor): 3 mm.
+
+screw hole size: 3 mm.
+
+cut out length (for sensor): 11 mm.
+cut out height (for sensor): 5 mm.
+
+
